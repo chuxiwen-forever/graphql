cf43b143b95fe3220944a403876356108cea4bde
chuxiwen <1609185446@qq.com>
2023-11-15 16:42:08 +0800

    前端工程添加导航栏

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,17 +4,21 @@ import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
 import AuthPage from "./pages/Auth";
 import EventPage from "./pages/Event";
 import BookingPage from "./pages/Booking";
+import MainNavigation from "./components/Navigation/MainNavigation";
 
 class App extends Component {
     render() {
         return (
             <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<Navigate to="/auth"/>}></Route>
-                    <Route path="/auth" element={<AuthPage/>}></Route>
-                    <Route path="/events" element={<EventPage/>}></Route>
-                    <Route path="/bookings" element={<BookingPage/>}></Route>
-                </Routes>
+                <MainNavigation/>
+                <main className="main-content">
+                    <Routes>
+                        <Route path="/" element={<Navigate to="/auth"/>}></Route>
+                        <Route path="/auth" element={<AuthPage/>}></Route>
+                        <Route path="/events" element={<EventPage/>}></Route>
+                        <Route path="/bookings" element={<BookingPage/>}></Route>
+                    </Routes>
+                </main>
             </BrowserRouter>
         )
     }

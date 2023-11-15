b5766425dac58c7f7f2f0af757f4483777d1b434
chuxiwen <1609185446@qq.com>
2023-11-15 16:51:08 +0800

    实现登录界面

--- frontend/src/pages/Auth.js
+++ frontend/src/pages/Auth.js
@@ -1,8 +1,23 @@
 import {Component} from "react";
+import "./Auth.css"
 
-class AuthPage extends Component{
+class AuthPage extends Component {
     render() {
-        return <h1>The Auth Page</h1>;
+        return (
+            <form className="auth-form">
+                <div className="form-control">
+                    <label htmlFor="email">E-Mail</label>
+                    <input type="email" id="email"/>
+                </div>
+                <div className="form-control">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" id="password"/>
+                </div>
+                <div className="form-actions">
+                    <button type="submit">Submit</button>
+                </div>
+            </form>
+        );
     }
 }
 

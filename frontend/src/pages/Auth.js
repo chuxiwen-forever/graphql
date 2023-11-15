180ce7d42a656b85da8abfef6d02d0b492065824
chuxiwen <1609185446@qq.com>
2023-11-15 17:07:27 +0800

    前端实现注册逻辑

--- frontend/src/pages/Auth.js
+++ frontend/src/pages/Auth.js
@@ -1,17 +1,68 @@
-import {Component} from "react";
+import React, {Component} from "react";
 import "./Auth.css"
 
 class AuthPage extends Component {
+    constructor(props) {
+        super(props);
+        this.emailEl = React.createRef();
+        this.passwordEl = React.createRef();
+    }
+
+    submitHandler = (event) => {
+        // 禁用自己提交功能
+        event.preventDefault();
+
+        const email = this.emailEl.current.value;
+        const password = this.passwordEl.current.value;
+
+        if (email.trim().length === 0 || password.trim().length === 0) {
+            return;
+        }
+
+        const requestBody = {
+            query:`
+            mutation{
+                createUser(userInput: {
+                    email: "${email}",
+                    password: "${password}"
+                }){
+                    id
+                    email
+                }
+            }
+            `,
+        };
+
+        fetch("http://localhost:8080/graphql", {
+            method: "POST",
+            body: JSON.stringify(requestBody),
+            headers: {
+                "Content-Type": "application/json",
+            }
+        }).then((res) => {
+                if (res.status !== 200 || res.status !== 201){
+                    throw new Error("Failed!");
+                }
+                return res.json();
+            })
+            .then((resData) => {
+                console.log(resData)
+            })
+            .catch((err) =>{
+            console.log(err)
+        })
+    };
+
     render() {
         return (
-            <form className="auth-form">
+            <form className="auth-form" onSubmit={this.submitHandler}>
                 <div className="form-control">
                     <label htmlFor="email">E-Mail</label>
-                    <input type="email" id="email"/>
+                    <input type="email" id="email" ref={this.emailEl}/>
                 </div>
                 <div className="form-control">
                     <label htmlFor="password">Password</label>
-                    <input type="password" id="password"/>
+                    <input type="password" id="password" ref={this.passwordEl}/>
                 </div>
                 <div className="form-actions">
                     <button type="submit">Submit</button>
